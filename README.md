bf7d8580e5be33d10777428e8f6aad3ecbb51315
hansent <thomas.hansen@gmail.com>
2015-03-31 01:20:01 -0500

    create basic game model also low fidelity playable ascii version using contentEditable

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 dotnbox
-==============
+===========
 
+## How to play
+
+ - the youngest player starts the game
+ - take turns picking horizontal edges ("--") or vertical edges ("|").
+ - to "make a line" for that edge, replace "--"" with "##", or "|" with "#" .
+ - if you close a box, write P1/P2 or your initials in the middle
+ - if you close a box, you also get to go again
+ - once all boxes are closed, player with the most boxes wins!
+
+
+"""
+    In this move, player two picks the edge on the second row from the bottom in the center column
+
+     * -- * ## * ## *        * -- * ## * ## *
+     |    |    # P1 #        |    |    # P1 #
+     * -- * ## * ## *        * -- * ## * ## *
+     |    #    #    |   ->   |    # P2 #    |
+     * ## * -- * -- *        * ## * ## * -- *
+     #    |    |    |        #    |    |    |
+     * -- * -- * -- *        * -- * -- * -- *
+"""
--- public/index.js
+++ public/index.js
@@ -1,25 +1,126 @@
 
+var $ = require('jquery');
+
+
+
 /**
- * creates an n x m grid to play dots and lines on.
- * @param {Number} n 
- * @param {Number} m
+ * create a 4x4 game board for playing dots and lines
  */
-var Grid = function(n, m){
-    this.width = n;
-    this.height = m;
+function Game(){
+    this.num_rows = 4;
+    this.num_columns = 4;
 
-    this.edges = [];
-    for (var x=0; x<n-1; x++){
-    for (var y=0; y<m-1; y++){
-            this.edges.push({
-                'from': {'x':x+1, 'y':y}, 
-                'to': {'x':x, 'y':y+1},
-                'player': 0,
-            });
+    // two dimensional array to address edges in 
+    // the graph / lines in the game
+    this.edges = [
+        [0,0,0],   // (3 edges)  * -- * -- * -- *  <- "vertex row"
+        [0,0,0,0], // (4 edges)  |    |    |    |  <- "edge row"
+        [0,0,0],   // (3 edges)  * -- * -- * -- *  <- "vertex row"
+        [0,0,0,0], // (4 edges)  |    |    |    |  <- "edge row"
+        [0,0,0],   // (3 edges)  * -- * -- * -- *  <- "vertex row"
+        [0,0,0,0], // (4 edges)  |    |    |    |  <- "edge row"
+        [0,0,0]    // (3 edges)  * -- * -- * -- *  <- "vertex row"
+    ];
+}
+
+
+
+
+/**
+ * renders the game board
+ * @param  {game}
+ * @return {string}
+ */
+function renderGameState(game){
+    var out = "";
+    for (var i=0; i<game.edges.length; i++){
+        var row = game.edges[i];
+        out += renderRow(row) + "\n";
     }
+    return out;
+}
+
+
+
+/**
+ * renders a row of the game board calls either
+ * renderVertexRow for even rows and VertexEdge
+ * on odd rows
+ * @param  {array}
+ * @return {string}
+ */
+function renderRow(row){
+    if (row.length%2)
+        return renderVertexRow(row);
+    else
+        return renderEdgeRow(row);
+}
+
+
+
+/**
+ * constructs string representation of a "vertex row" 
+ * like e.g.: "* -- * -- * -- *"
+ * 
+ * @param  {array} edges  an array of edge states
+ * @return {string} 
+ */
+function renderVertexRow(edges){
+    // we render " ## " instead of " -- "
+    // to indicate lines placed by players 
+    
+    // e.g.: "* -- * ## * -- *"  indicates
+    // that the line was placed in the middle
+    var out = "*"; 
+    for (var i=0; i<edges.length; i++){
+        if (edges[i])
+            out += " ## *";
+        else
+            out += " -- *";
     }
-};
+    return out;
+}
 
 
-g = new Grid(4,4);
-console.log(g);
+
+/**
+ * construct string representation of edge rows
+ * like e.g.: "|    |    |    |"
+ * 
+ * @param  {array} edges  an array of edge states
+ * @return {string} 
+ */
+function renderEdgeRow(edges){
+    // we render "#" instead of "|"
+    // to indicate lines placed by players 
+
+    // e.g.: "#    #    |    |" if the two lef most
+    // edges had "lines placed on them"
+    var out = ""; 
+    for (var i=0; i<edges.length; i++){
+        if (edges[i])
+            out += "#";
+        else
+            out += "|";
+
+        if (i < edges.length-1) // all but last one
+            out += "    ";
+    }
+    return out;
+}
+
+
+
+
+
+
+$(function(){
+    var game = new Game();
+    ascii_board = renderGameState(game)
+    $('#game').html("<pre>" + ascii_board + "</pre>");
+    console.log(ascii_board);
+})
+
+
+
+
